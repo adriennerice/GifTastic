2f95a6a72670143b39443b3ef2bb2a3802ce5f3e
Adrienne Rice <adrienne.rice@gmail.com>
2018-11-02 21:19:36 -0400

    logic complete

---
+++ javascript/script.js
@@ -0,0 +1,89 @@
+var cartoons = ["Adventure Time", "Archer", "Futurama", "X-Men"];
+
+function displayCartoonInfo(){
+    $("#cartoon-view").empty();
+
+    var cartoon = $(this).attr("data-name");
+
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + cartoon + "&api_key=oRrzUVJuSNpp7zOSUYJQ0dihn858LVw8&limit=10";
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response){
+        console.log(queryURL);
+
+        console.log(response);
+
+        var results = response.data;
+
+        for (var i = 0; i < results.length; i++){
+            var cartoonDiv=$("<div>");
+
+            var p = $("<p>").text("Rating: " + results[i].rating);
+
+            var cartoonImage = $("<img>");
+
+            cartoonImage.attr("src", results[i].images.fixed_height_still.url);
+            cartoonImage.attr("still_src", results[i].images.fixed_height_still.url);
+            cartoonImage.attr("animated_src", results[i].images.fixed_height.url);
+
+
+            cartoonImage.addClass("gif");
+            cartoonImage.attr("data-state", "still");
+            
+            cartoonDiv.append(p);
+            cartoonDiv.append(cartoonImage);
+
+            $("#cartoon-view").prepend(cartoonDiv);
+        }
+    });
+};
+
+function renderButtons(){
+  $("#buttons-view").empty();
+  
+  for (var i=0; i < cartoons.length; i++){
+      var a = $("<button>");
+
+      a.addClass("cartoon-btn");
+
+      a.attr("data-name", cartoons[i]);
+
+      a.text(cartoons[i]);
+
+      $("#buttons-view").append(a);
+  }
+};
+
+function stillToanimate(){
+    var imageState = $(this).attr("data-state");
+
+    if(imageState === "still"){
+        $(this).attr("src", $(this).attr("animated_src"));
+        $(this).attr("data-state", "animate");
+        
+    } else {
+        $(this).attr("src", $(this).attr("still_src"));
+        $(this).attr("dta-state", "still");
+        
+    }
+};
+
+
+
+
+$("#add-cartoon").on("click", function(){
+    event.preventDefault();
+
+    var cartoon=$("#cartoon-input").val().trim();
+     
+    cartoons.push(cartoon);
+
+    renderButtons();
+});
+
+$(document).on("click", ".cartoon-btn", displayCartoonInfo);
+$(document).on("click", ".gif", stillToanimate);
+
+renderButtons();
